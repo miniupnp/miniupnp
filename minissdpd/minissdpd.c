fd96a3ddcdbe1157c6b71ab3e7c051522601b9a2
Thomas Bernard <miniupnp@free.fr>
2011-10-07 11:29:22 +0200

    minissdpd: unlink unix socket before binding
    see http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=644510

--- minissdpd/minissdpd.c
+++ minissdpd/minissdpd.c
@@ -1,4 +1,4 @@
-/* $Id: minissdpd.c,v 1.24 2011/07/30 13:10:05 nanard Exp $ */
+/* $Id: minissdpd.c,v 1.25 2011/10/07 09:13:25 nanard Exp $ */
 /* MiniUPnP project
  * (c) 2007-2011 Thomas Bernard
  * website : http://miniupnp.free.fr/ or http://miniupnp.tuxfamily.org/
@@ -462,10 +462,19 @@ OpenUnixSocket(const char * path)
 {
 	struct sockaddr_un addr;
 	int s;
+	int rv;
 	s = socket(AF_UNIX, SOCK_STREAM, 0);
 	if(s < 0)
 	{
 		syslog(LOG_ERR, "socket(AF_UNIX): %m");
+		return -1;
+	}
+	/* unlink the socket pseudo file before binding */
+	rv = unlink(path);
+	if(rv < 0 && errno != ENOENT)
+	{
+		syslog(LOG_ERR, "unlink(unixsocket, \"%s\"): %m", path);
+		close(s);
 		return -1;
 	}
 	addr.sun_family = AF_UNIX;
