9fb0a2ae2da563880a7557b634e1823f0c2ef323
Thomas Bernard <miniupnp@free.fr>
2012-02-13 22:00:11 +0100

    update version in miniupnpc/setup*.py

--- miniupnpc/setupmingw32.py
+++ miniupnpc/setupmingw32.py
@@ -1,12 +1,12 @@
 #! /usr/bin/python
-# $Id: setupmingw32.py,v 1.5 2011/05/15 21:18:43 nanard Exp $
-# the MiniUPnP Project (c) 2007-2011 Thomas Bernard
+# $Id: setupmingw32.py,v 1.6 2012/02/13 20:58:57 nanard Exp $
+# the MiniUPnP Project (c) 2007-2012 Thomas Bernard
 # http://miniupnp.tuxfamily.org/ or http://miniupnp.free.fr/
 #
 # python script to build the miniupnpc module under windows (using mingw32)
 #
 from distutils.core import setup, Extension
-setup(name="miniupnpc", version="1.5",
+setup(name="miniupnpc", version="1.6",
       ext_modules=[
 	         Extension(name="miniupnpc", sources=["miniupnpcmodule.c"],
 	                   libraries=["ws2_32", "iphlpapi"],
