38939ac2d99a68a801b12af88a970c6d11c94f5f
Thomas Bernard <miniupnp@free.fr>
2012-10-05 00:32:04 +0200

    miniupnpd/upnpevents.c: Better error checking after connect()

--- miniupnpd/upnpevents.c
+++ miniupnpd/upnpevents.c
@@ -426,9 +426,23 @@ static void upnp_event_recv(struct upnp_event_notify * obj)
 static void
 upnp_event_process_notify(struct upnp_event_notify * obj)
 {
+	int err;
+	socklen_t len;
 	switch(obj->state) {
 	case EConnecting:
 		/* now connected or failed to connect */
+		len = sizeof(err);
+		if(getsockopt(obj->s, SOL_SOCKET, SO_ERROR, &err, &len) < 0) {
+			syslog(LOG_ERR, "%s: getsockopt: %m", "upnp_event_process_notify");
+			obj->state = EError;
+			break;
+		}
+		if(err != 0) {
+			errno = err;
+			syslog(LOG_WARNING, "%s: connect failed: %m", "upnp_event_process_notify");
+			obj->state = EError;
+			break;
+		}
 		upnp_event_prepare(obj);
 		if(obj->state == ESending)
 			upnp_event_send(obj);
@@ -444,7 +458,7 @@ upnp_event_process_notify(struct upnp_event_notify * obj)
 		obj->s = -1;
 		break;
 	default:
-		syslog(LOG_ERR, "upnp_event_process_notify: unknown state");
+		syslog(LOG_ERR, "%s: unknown state", "upnp_event_process_notify");
 	}
 }
 
