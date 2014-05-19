126bf58d23798f1061076b6febb302f3b9296dc4
Thomas Bernard <miniupnp@free.fr>
2014-05-20 00:24:59 +0200

    miniupnpc: add more info to python packages

--- miniupnpc/setup.py
+++ miniupnpc/setup.py
@@ -10,9 +10,15 @@ from distutils.core import setup, Extension
 from distutils import sysconfig
 sysconfig.get_config_vars()["OPT"] = ''
 sysconfig.get_config_vars()["CFLAGS"] = ''
-setup(name="miniupnpc", version="1.7",
+setup(name="miniupnpc",
+      version=open('VERSION').read().strip(),
+      author='Thomas BERNARD',
+      author_email='miniupnp@free.fr',
+      license=open('LICENSE').read(),
+      url='http://miniupnp.free.fr/',
+      description='miniUPnP client',
       ext_modules=[
-	         Extension(name="miniupnpc", sources=["miniupnpcmodule.c"],
-			           extra_objects=["libminiupnpc.a"])
-			 ])
+         Extension(name="miniupnpc", sources=["miniupnpcmodule.c"],
+                   extra_objects=["libminiupnpc.a"])
+      ])
 
