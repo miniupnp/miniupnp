0298b6636584a3e18c96c5165824e4eb120b0668
Daniel Becker <razzfazz@gmail.com>
2015-12-11 23:12:11 -0800

    fix argument names for DeviceProtection actions

--- miniupnpd/upnpdescgen.c
+++ miniupnpd/upnpdescgen.c
@@ -115,7 +115,12 @@ static const char * magicargname[] = {
 	"RemotePort",
 	"InternalClient",
 	"InternalPort",
-	"IsWorking"
+	"IsWorking",
+	"ProtocolType",
+	"InMessage",
+	"OutMessage",
+	"ProtocolList",
+	"RoleList"
 };
 
 static const char xmlver[] =
@@ -746,21 +751,21 @@ static const struct serviceDesc scpd6FC =
 
 static const struct argument SendSetupMessageArgs[] =
 {
-	{1|0x80, 6},	/* ProtocolType */
-	{1|0x80, 5},	/* InMessage */
-	{2|0x80, 5},	/* OutMessage */
+	{1|0x80|(8<<2), 6},	/* ProtocolType */
+	{1|0x80|(9<<2), 5},	/* InMessage */
+	{2|0x80|(10<<2), 5},	/* OutMessage */
 	{0, 0}
 };
 
 static const struct argument GetSupportedProtocolsArgs[] =
 {
-	{2, 1},	/* ProtocolList */
+	{2|0x80|(11<<2), 1},	/* ProtocolList */
 	{0, 0}
 };
 
 static const struct argument GetAssignedRolesArgs[] =
 {
-	{2, 6},	/* RoleList */
+	{2|0x80|(12<<2), 6},	/* RoleList */
 	{0, 0}
 };
 
