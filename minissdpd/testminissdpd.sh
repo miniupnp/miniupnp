78ab294de9a10481989dde9f46254062b736bb4f
Thomas Bernard <miniupnp@free.fr>
2016-01-13 11:11:47 +0100

    testminissdpd.sh: allow to specify the network interface on command line

--- minissdpd/testminissdpd.sh
+++ minissdpd/testminissdpd.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # $Id: testminissdpd.sh,v 1.2 2015/09/03 18:31:25 nanard Exp $
-# (c) 2015 Thomas Bernard
+# (c) 2016 Thomas Bernard
 
 OS=`uname -s`
 IF=lo
 if [ "$OS" = "Darwin" ] ; then
 	IF=lo0
+fi
+# if set, 1st argument is network interface
+if [ -n "$1" ] ; then
+	IF=$1
 fi
 SOCKET=`mktemp -t minissdpdsocketXXXXXX`
 PID="${SOCKET}.pid"
