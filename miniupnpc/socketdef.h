6900009406f0f3d7e5513cef690348928aa3d3ce
irwir <irwir@users.noreply.github.com>
2018-03-08 14:50:48 +0300

    ocketdef.h header fixed

--- miniupnpc/socketdef.h
+++ miniupnpc/socketdef.h
@@ -3,8 +3,12 @@
 #ifdef _MSC_VER
 #define ISINVALID(s) (INVALID_SOCKET==(s))
 #else
-typedef SOCKET int
-typedef SSIZE_T ssize_t
+typedef SOCKET int;
+typedef SSIZE_T ssize_t;
+#ifndef INVALID_SOCKET
 #define INVALID_SOCKET (-1)
+#endif
+#ifndef ISINVALID
 #define ISINVALID(s) (0>(s))
+#endif
 #endif
