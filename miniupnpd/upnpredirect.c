a92138345b609214095f2ed8b12301989aa50e47
Thomas Bernard <miniupnp@free.fr>
2018-05-02 09:40:12 +0200

    add option LEASEFILE_USE_REMAINING_TIME
    new function lease_file_rewrite()
    that is called just before exiting, and when SIGUSR2 is received
    see #295

--- miniupnpd/upnpredirect.c
+++ miniupnpd/upnpredirect.c
@@ -295,6 +295,31 @@ int reload_from_lease_file()
 
 	return 0;
 }
+
+#ifdef LEASEFILE_USE_REMAINING_TIME
+void lease_file_rewrite(void)
+{
+	int index;
+	unsigned short eport, iport;
+	int proto;
+	char iaddr[32];
+	char desc[64];
+	char rhost[40];
+	unsigned int timestamp;
+
+	if (lease_file == NULL) return;
+	remove(lease_file);
+	for(index = 0; ; index++) {
+		if(get_redirect_rule_by_index(index, 0/*ifname*/, &eport, iaddr, sizeof(iaddr),
+		                              &iport, &proto, desc, sizeof(desc),
+		                              rhost, sizeof(rhost), &timestamp,
+		                              0, 0) < 0)
+			break;
+		if(lease_file_add(eport, iaddr, iport, proto, desc, timestamp) < 0)
+			break;
+	}
+}
+#endif
 #endif
 
 /* upnp_redirect()
