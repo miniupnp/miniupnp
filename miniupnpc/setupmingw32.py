b11f642b819b4c4a38cfc6623dbace279588f975
Nathan Richard <nathan.richard@live.fr>
2019-04-25 17:11:53 +0200

    Creation a build matrix to support various python versions.

--- miniupnpc/setupmingw32.py
+++ miniupnpc/setupmingw32.py
@@ -6,6 +6,13 @@
 #
 # python script to build the miniupnpc module under windows (using mingw32)
 #
+import sys
+
+if (sys.version_info.major * 10 +  sys.version_info.minor) >= 35:
+        compat_lib = ["legacy_stdio_definitions"]
+else:
+        compat_lib = []
+
 try:
         from setuptools import setup, Extension
 except ImportError:
@@ -22,7 +29,7 @@ setup(name="miniupnpc",
       description='miniUPnP client',
       ext_modules=[
          Extension(name="miniupnpc", sources=["miniupnpcmodule.c"],
-                   libraries=["ws2_32", "iphlpapi", "legacy_stdio_definitions"],
+                   libraries=["ws2_32", "iphlpapi"] + compat_lib,
                    extra_objects=["libminiupnpc.a"])
       ])
 
