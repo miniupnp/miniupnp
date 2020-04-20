07abee862c2b7ade362042bffe8d1a2146a1ac0e
Thomas Bernard <miniupnp@free.fr>
2020-04-20 23:37:24 +0200

    miniupnpd: Fix "IGD2 Port Triggering" in update_portmapping()

--- miniupnpd/netfilter/iptcrdr.c
+++ miniupnpd/netfilter/iptcrdr.c
@@ -2,7 +2,7 @@
 /* vim: tabstop=4 shiftwidth=4 noexpandtab
  * MiniUPnP project
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
- * (c) 2006-2019 Thomas Bernard
+ * (c) 2006-2020 Thomas Bernard
  * This software is subject to the conditions detailed
  * in the LICENCE file provided within the distribution */
 #include <stdio.h>
