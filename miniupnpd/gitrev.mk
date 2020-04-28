2a8368a2de18458c39601ce50aa2fae40dd36085
Thomas Bernard <miniupnp@free.fr>
2020-04-29 01:49:06 +0200

    gitrev.mk: CFLAGS => CPPFLAGS

--- miniupnpd/gitrev.mk
+++ miniupnpd/gitrev.mk
@@ -1,9 +1,9 @@
-# (c) 2019 Thomas Bernard
+# (c) 2019-2020 Thomas Bernard
 # For GNU Make
 
 ISGITREPO := $(shell git rev-parse --is-inside-work-tree)
 ifeq ($(ISGITREPO),true)
 GITREF := $(shell git rev-parse --short HEAD)
 GITBRANCH := $(shell git rev-parse --abbrev-ref HEAD)
-CFLAGS += -DMINIUPNPD_GIT_REF=\"$(GITBRANCH)-$(GITREF)\"
+CPPFLAGS += -DMINIUPNPD_GIT_REF=\"$(GITBRANCH)-$(GITREF)\"
 endif
