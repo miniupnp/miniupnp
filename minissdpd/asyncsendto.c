39ff47ad79537df20baff65a90174f3b05229e2c
Thomas Bernard <miniupnp@free.fr>
2020-06-06 21:54:58 +0200

    update  minissdpd/asyncsendto.c

--- minissdpd/asyncsendto.c
+++ minissdpd/asyncsendto.c
@@ -1,11 +1,12 @@
-/* $Id: asyncsendto.c,v 1.10 2018/07/06 11:44:59 nanard Exp $ */
+/* $Id: asyncsendto.c,v 1.11 2019/09/24 11:46:01 nanard Exp $ */
 /* MiniUPnP project
  * http://miniupnp.free.fr/ or http://miniupnp.tuxfamily.org/
- * (c) 2006-2018 Thomas Bernard
+ * (c) 2006-2019 Thomas Bernard
  * This software is subject to the conditions detailed
  * in the LICENCE file provided within the distribution */
 
 #include <sys/types.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/queue.h>
