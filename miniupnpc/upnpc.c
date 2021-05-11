0c7579c9aab6326f79464a02d063b316e06786ce
Thomas Bernard <miniupnp@free.fr>
2021-05-12 00:00:06 +0200

    2020 => 2021

--- miniupnpc/upnpc.c
+++ miniupnpc/upnpc.c
@@ -1,7 +1,7 @@
-/* $Id: upnpc.c,v 1.126 2020/11/09 19:38:06 nanard Exp $ */
+/* $Id: upnpc.c,v 1.129 2021/05/10 20:51:29 nanard Exp $ */
 /* Project : miniupnp
  * Author : Thomas Bernard
- * Copyright (c) 2005-2020 Thomas Bernard
+ * Copyright (c) 2005-2021 Thomas Bernard
  * This software is subject to the conditions detailed in the
  * LICENCE file provided in this distribution. */
 
@@ -583,7 +583,7 @@ int main(int argc, char ** argv)
 	}
 #endif
     printf("upnpc : miniupnpc library test client, version %s.\n", MINIUPNPC_VERSION_STRING);
-	printf(" (c) 2005-2020 Thomas Bernard.\n");
+	printf(" (c) 2005-2021 Thomas Bernard.\n");
     printf("Go to http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/\n"
 	       "for more information.\n");
 	/* command line processing */
