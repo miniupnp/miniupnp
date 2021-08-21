92cf5c2f9540539a77782a72c4d363c5ad1913a6
Thomas Bernard <miniupnp@free.fr>
2021-08-21 10:26:31 +0200

    nftnlrdr_misc.c: 2021

--- miniupnpd/netfilter_nft/nftnlrdr_misc.c
+++ miniupnpd/netfilter_nft/nftnlrdr_misc.c
@@ -1,10 +1,10 @@
-/* $Id: nftnlrdr_misc.c,v 1.9 2020/05/29 16:09:21 nanard Exp $ */
+/* $Id: nftnlrdr_misc.c,v 1.14 2021/08/21 08:24:38 nanard Exp $ */
 /* vim: tabstop=4 shiftwidth=4 noexpandtab
  * MiniUPnP project
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
  * (c) 2015 Tomofumi Hayashi
  * (c) 2019 Paul Chambers
- * (c) 2019-2020 Thomas Bernard
+ * (c) 2019-2021 Thomas Bernard
  *
  * This software is subject to the conditions detailed
  * in the LICENCE file provided within the distribution.
