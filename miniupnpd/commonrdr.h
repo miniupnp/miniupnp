af0ee582d9f664537e1d50313731dc8f1c269f9a
Thomas Bernard <miniupnp@free.fr>
2021-12-01 23:53:07 +0100

    commonrdr.h: 2021

--- miniupnpd/commonrdr.h
+++ miniupnpd/commonrdr.h
@@ -1,6 +1,6 @@
-/* $Id: commonrdr.h,v 1.11 2019/10/02 22:12:02 nanard Exp $ */
+/* $Id: commonrdr.h,v 1.15 2021/12/01 22:50:09 nanard Exp $ */
 /* MiniUPnP project
- * (c) 2006-2020 Thomas Bernard
+ * (c) 2006-2021 Thomas Bernard
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
  * This software is subject to the conditions detailed
  * in the LICENCE file provided within the distribution */
