bbe7d1355e8c1bd0f5ac64de9ee29cd051ef90b4
Thomas Bernard <miniupnp@free.fr>
2024-01-15 01:01:16 +0100

    minissdp.c: 2024

--- miniupnpd/minissdp.c
+++ miniupnpd/minissdp.c
@@ -1,8 +1,8 @@
-/* $Id: minissdp.c,v 1.106 2023/06/29 09:45:18 nanard Exp $ */
+/* $Id: minissdp.c,v 1.107 2024/01/15 00:20:21 nanard Exp $ */
 /* vim: tabstop=4 shiftwidth=4 noexpandtab
  * MiniUPnP project
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
- * (c) 2006-2023 Thomas Bernard
+ * (c) 2006-2024 Thomas Bernard
  * This software is subject to the conditions detailed
  * in the LICENCE file provided within the distribution */
 
