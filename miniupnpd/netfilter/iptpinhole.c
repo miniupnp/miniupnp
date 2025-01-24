5ba9671b9cb9584bf890f21d62dc86260aa0c2a1
Thomas BERNARD <miniupnp@free.fr>
2025-01-25 00:56:39 +0100

    iptpinhole.c: prevent warning in call to ip6tc_append_entry() and ip6tc_delete_num_entry()
    fixes #782
    
    In function ‘ip6tc_init_verify_append’,
        inlined from ‘add_pinhole’ at ../netfilter/iptpinhole.c:271:5:
    ../netfilter/iptpinhole.c:183:13: warning: ‘ip6tc_append_entry’ reading 32 bytes from a region of size 10 [-Wstringop-overread]
      183 |         if(!ip6tc_append_entry(chain, e, h)) {
          |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../netfilter/iptpinhole.c: In function ‘add_pinhole’:
    ../netfilter/iptpinhole.c:183:13: note: referencing argument 1 of type ‘const char *’
    In file included from ../netfilter/iptpinhole.c:24:
    /usr/include/libiptc/libip6tc.h:74:5: note: in a call to function ‘ip6tc_append_entry’
       74 | int ip6tc_append_entry(const xt_chainlabel chain,
          |     ^~~~~~~~~~~~~~~~~~
    ../netfilter/iptpinhole.c: In function ‘delete_pinhole’:
    ../netfilter/iptpinhole.c:353:37: warning: ‘ip6tc_delete_num_entry’ reading 32 bytes from a region of size 10 [-Wstringop-overread]
      353 |                                 if(!ip6tc_delete_num_entry(miniupnpd_v6_filter_chain, index, h)) {
          |                                     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../netfilter/iptpinhole.c:353:37: note: referencing argument 1 of type ‘const char *’
    In file included from ../netfilter/iptpinhole.c:24:
    /usr/include/libiptc/libip6tc.h:91:5: note: in a call to function ‘ip6tc_delete_num_entry’
       91 | int ip6tc_delete_num_entry(const xt_chainlabel chain,
          |     ^~~~~~~~~~~~~~~~~~~~~~

--- miniupnpd/netfilter/iptpinhole.c
+++ miniupnpd/netfilter/iptpinhole.c
@@ -170,17 +170,19 @@ ip6tc_init_verify_append(const char * table,
                          struct ip6t_entry * e)
 {
 	IP6TC_HANDLE h;
+	xt_chainlabel chainlabel;
 
 	h = ip6tc_init(table);
 	if(!h) {
 		syslog(LOG_ERR, "ip6tc_init error : %s", ip6tc_strerror(errno));
 		return -1;
 	}
-	if(!ip6tc_is_chain(chain, h)) {
+	strncpy(chainlabel, chain, sizeof(chainlabel));
+	if(!ip6tc_is_chain(chainlabel, h)) {
 		syslog(LOG_ERR, "chain %s not found", chain);
 		goto error;
 	}
-	if(!ip6tc_append_entry(chain, e, h)) {
+	if(!ip6tc_append_entry(chainlabel, e, h)) {
 		syslog(LOG_ERR, "ip6tc_append_entry() error : %s", ip6tc_strerror(errno));
 		goto error;
 	}
@@ -325,6 +327,7 @@ delete_pinhole(unsigned short uid)
 	const struct ip6t_entry_match *match = NULL;
 	/*const struct ip6t_entry_target *target = NULL;*/
 	unsigned int index;
+	xt_chainlabel chainlabel;
 
 	p = get_pinhole(uid);
 	if(!p)
@@ -335,12 +338,13 @@ delete_pinhole(unsigned short uid)
 		syslog(LOG_ERR, "ip6tc_init error : %s", ip6tc_strerror(errno));
 		return -1;
 	}
-	if(!ip6tc_is_chain(miniupnpd_v6_filter_chain, h)) {
+	strncpy(chainlabel, miniupnpd_v6_filter_chain, sizeof(chainlabel));
+	if(!ip6tc_is_chain(chainlabel, h)) {
 		syslog(LOG_ERR, "chain %s not found", miniupnpd_v6_filter_chain);
 		goto error;
 	}
 	index = 0;
-	for(e = ip6tc_first_rule(miniupnpd_v6_filter_chain, h);
+	for(e = ip6tc_first_rule(chainlabel, h);
 	    e;
 	    e = ip6tc_next_rule(e, h)) {
 		if((e->ipv6.proto == p->proto) &&
@@ -350,7 +354,7 @@ delete_pinhole(unsigned short uid)
 			match = (const struct ip6t_entry_match *)&e->elems;
 			info = (const struct ip6t_tcp *)&match->data;
 			if((info->spts[0] == p->sport) && (info->dpts[0] == p->dport)) {
-				if(!ip6tc_delete_num_entry(miniupnpd_v6_filter_chain, index, h)) {
+				if(!ip6tc_delete_num_entry(chainlabel, index, h)) {
 					syslog(LOG_ERR, "ip6tc_delete_num_entry(%s,%u,...): %s",
 					       miniupnpd_v6_filter_chain, index, ip6tc_strerror(errno));
 					goto error;
