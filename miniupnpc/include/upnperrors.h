16db6cd2d4d9cb5e50089e1845830d2c095b9572
Thomas Bernard <miniupnp@free.fr>
2025-01-26 01:32:35 +0100

    Doxygen comments for igd_desc_parse.h / upnpdev.h / upnperrors.h

--- miniupnpc/include/upnperrors.h
+++ miniupnpc/include/upnperrors.h
@@ -1,22 +1,31 @@
-/* $Id: upnperrors.h,v 1.2 2008/07/02 23:31:15 nanard Exp $ */
-/* (c) 2007-2015 Thomas Bernard
+/* $Id: upnperrors.h,v 1.7 2025/01/26 00:31:44 nanard Exp $ */
+/* (c) 2007-2025 Thomas Bernard
  * All rights reserved.
  * MiniUPnP Project.
- * http://miniupnp.free.fr/ or http://miniupnp.tuxfamily.org/
+ * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
  * This software is subjet to the conditions detailed in the
  * provided LICENCE file. */
 #ifndef UPNPERRORS_H_INCLUDED
 #define UPNPERRORS_H_INCLUDED
 
+/*! \file upnperrors.h
+ * \brief code to string API for errors
+ */
+/*! \cond */
 #include "miniupnpc_declspec.h"
+/*! \endcond */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* strupnperror()
- * Return a string description of the UPnP error code
- * or NULL for undefinded errors */
+/*!
+ * \brief convert error code to string
+ *
+ * \param[in] err error code
+ * \return a string description of the error code
+ *         or NULL for undefinded errors
+ */
 MINIUPNP_LIBSPEC const char * strupnperror(int err);
 
 #ifdef __cplusplus
