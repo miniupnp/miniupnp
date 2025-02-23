64018a6f221321da7572c6709a701a9aa3ad714a
Thomas Bernard <miniupnp@free.fr>
2025-02-23 20:55:48 +0100

    move parseURL() to miniwget_private.h

--- miniupnpc/include/miniwget.h
+++ miniupnpc/include/miniwget.h
@@ -47,31 +47,6 @@ MINIUPNP_LIBSPEC void * miniwget_getaddr(const char * url, int * size,
                                          char * addr, int addrlen,
                                          unsigned int scope_id, int * status_code);
 
-/*! \brief parse a HTTP URL
- *
- * URL formats supported :
- * - `http://192.168.1.1/path/xxx`
- * - `http://192.168.1.1:8080/path/xxx`
- * - `http://[2a00:1234:5678:90ab::123]/path/xxx`
- * - `http://[2a00:1234:5678:90ab::123]:8080/path/xxx`
- * - `http://[fe80::1234:5678:90ab%%eth0]/path/xxx`
- * - `http://[fe80::1234:5678:90ab%%eth0]:8080/path/xxx`
- *
- * `%` may be encoded as `%25`
- *
- * \todo should probably not be exposed in the public API
- * \param[in] url URL to parse
- * \param[out] hostname hostname part of the URL (size of MAXHOSTNAMELEN+1)
- * \param[out] port set to the port specified in the URL or 80
- * \param[out] path set to the begining of the path part of the URL
- * \param[out] scope_id set to the interface id if specified in the
- *             link-local IPv6 address
- * \return 0 for failure, 1 for success
- */
-int parseURL(const char * url,
-             char * hostname, unsigned short * port, char * * path,
-             unsigned int * scope_id);
-
 #ifdef __cplusplus
 }
 #endif
