miniupnp/miniupnp

commit 7fc5b4d2a5e62eb9152441715922b9083c19ed12

miniupnpd: improve get_wan_connection_status() and getifaddr()

miniupnpd/testgetifaddr.c
@@ -1,7 +1,7 @@
-/* $Id: testgetifaddr.c,v 1.7 2013/04/27 15:38:57 nanard Exp $ */
+/* $Id: testgetifaddr.c,v 1.9 2025/04/08 21:28:45 nanard Exp $ */
 /* MiniUPnP project
  * http://miniupnp.free.fr/ or http://miniupnp.tuxfamily.org/
- * (c) 2006-2014 Thomas Bernard
+ * (c) 2006-2025 Thomas Bernard
  * This software is subject to the conditions detailed
  * in the LICENCE file provided within the distribution */
 #include <stdio.h>
@@ -12,6 +12,7 @@
 #include <arpa/inet.h>
 #include "config.h"
 #include "getifaddr.h"
+#include "getconnstatus.h"
 
 #if defined(__sun)
 /* solaris 10 does not define LOG_PERROR */
@@ -32,8 +33,11 @@ int main(int argc, char * * argv) {
 	}
 
 	openlog("testgetifaddr", LOG_CONS|LOG_PERROR, LOG_USER);
-	if(getifaddr(argv[1], str_addr, sizeof(str_addr), &addr, &mask) < 0) {
-		fprintf(stderr, "Cannot get address for interface %s.\n", argv[1]);
+	printf("Interface %s status : %s\n", argv[1], get_wan_connection_status_str(argv[1]));
+
+	r = getifaddr(argv[1], str_addr, sizeof(str_addr), &addr, &mask);
+	if(r < 0) {
+		fprintf(stderr, "getifaddr(\"%s\") returned %d\n", argv[1], r);
 		return 1;
 	}
 	printf("Interface %s has IP address %s.\n", argv[1], str_addr);

miniupnpd/Makefile.bsd
@@ -1,4 +1,4 @@
-# $Id: Makefile.bsd,v 1.112 2025/04/06 22:46:24 nanard Exp $
+# $Id: Makefile.bsd,v 1.113 2025/04/08 21:28:42 nanard Exp $
 # MiniUPnP project
 # http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
 # Author: Thomas Bernard
@@ -96,7 +96,7 @@ ALLSRCS += $(IPFOBJS:S/^/$(SRCDIR)\/ipf\//:S/.o$/.c/)
 
 TESTUPNPDESCGENOBJS = testupnpdescgen.o upnpdescgen.o
 TESTUPNPPERMISSIONSOBJS = testupnppermissions.o upnppermissions.o
-TESTGETIFADDROBJS = testgetifaddr.o getifaddr.o
+TESTGETIFADDROBJS = testgetifaddr.o getifaddr.o getconnstatus.o
 MINIUPNPDCTLOBJS = miniupnpdctl.o
 TESTASYNCSENDTOOBJS = testasyncsendto.o asyncsendto.o upnputils.o getroute.o
 TESTPORTINUSEOBJS = testportinuse.o portinuse.o getifaddr.o

miniupnpd/common.mk
@@ -8,7 +8,7 @@ testgetifstats:	testgetifstats.o getifstats.o
 
 testupnppermissions:	testupnppermissions.o upnppermissions.o
 
-testgetifaddr:	testgetifaddr.o getifaddr.o
+testgetifaddr:	testgetifaddr.o getifaddr.o getconnstatus.o
 
 testgetroute:	testgetroute.o getroute.o upnputils.o
 

miniupnpd/getconnstatus.c
@@ -1,4 +1,4 @@
-/* $Id: getconnstatus.c,v 1.7 2025/04/03 21:11:35 nanard Exp $ */
+/* $Id: getconnstatus.c,v 1.8 2025/04/08 21:28:42 nanard Exp $ */
 /* MiniUPnP project
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
  * (c) 2011-2025 Thomas Bernard
@@ -11,16 +11,21 @@
 #include "getconnstatus.h"
 #include "getifaddr.h"
 
+/**
+ * Only #STATUS_UNCONFIGURED, #STATUS_DISCONNECTED and #STATUS_CONNECTED
+ */
 int
 get_wan_connection_status(const char * ifname)
 {
-	char addr[INET_ADDRSTRLEN];
-	int r;
-
-	/*! \todo we need a better implementation here.
-	 * I'm afraid it should be device specific */
-	r = getifaddr(ifname, addr, INET_ADDRSTRLEN, NULL, NULL);
-	return (r < 0) ? STATUS_DISCONNECTED : STATUS_CONNECTED;
+	switch(getifaddr(ifname, NULL, 0, NULL, NULL)) {
+	case GETIFADDR_OK:
+		return STATUS_CONNECTED;
+	case GETIFADDR_NO_ADDRESS:
+	case GETIFADDR_IF_DOWN:
+		return STATUS_DISCONNECTED;
+	default:
+		return STATUS_UNCONFIGURED;
+	}
 }
 
 const char *
@@ -31,22 +36,22 @@ get_wan_connection_status_str(const char * ifname)
 
 	status = get_wan_connection_status(ifname);
 	switch(status) {
-	case 0:
+	case STATUS_UNCONFIGURED:
 		str = "Unconfigured";
 		break;
-	case 1:
+	case STATUS_CONNECTING:
 		str = "Connecting";
 		break;
-	case 2:
+	case STATUS_CONNECTED:
 		str = "Connected";
 		break;
-	case 3:
+	case STATUS_PENDINGDISCONNECT:
 		str = "PendingDisconnect";
 		break;
-	case 4:
+	case STATUS_DISCONNECTING:
 		str = "Disconnecting";
 		break;
-	case 5:
+	case STATUS_DISCONNECTED:
 		str = "Disconnected";
 		break;
 	}

miniupnpd/getifaddr.c
@@ -1,8 +1,8 @@
-/* $Id: getifaddr.c,v 1.28 2022/02/19 18:58:25 nanard Exp $ */
+/* $Id: getifaddr.c,v 1.31 2025/04/08 21:28:42 nanard Exp $ */
 /* vim: tabstop=4 shiftwidth=4 noexpandtab
  * MiniUPnP project
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
- * (c) 2006-2024 Thomas Bernard
+ * (c) 2006-2025 Thomas Bernard
  * This software is subject to the conditions detailed
  * in the LICENCE file provided within the distribution */
 
@@ -16,6 +16,7 @@
 #include <net/if.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 #if defined(sun)
 #include <sys/sockio.h>
 #endif
@@ -33,62 +34,66 @@ getifaddr(const char * ifname, char * buf, int len,
 #ifndef USE_GETIFADDRS
 	/* use ioctl SIOCGIFADDR. Works only for ip v4 */
 	/* SIOCGIFADDR struct ifreq *  */
-	int s;
+	int s, r;
 	struct ifreq ifr;
 	int ifrlen;
 	struct sockaddr_in * ifaddr;
 	ifrlen = sizeof(ifr);
 
 	if(!ifname || ifname[0]=='\0')
-		return -1;
+		return GETIFADDR_BAD_ARGS;
 	s = socket(PF_INET, SOCK_DGRAM, 0);
-	if(s < 0)
-	{
+	if(s < 0) {
 		syslog(LOG_ERR, "socket(PF_INET, SOCK_DGRAM): %m");
-		return -1;
+		return GETIFADDR_SOCKET_ERROR;
 	}
 	strncpy(ifr.ifr_name, ifname, IFNAMSIZ-1);
 	ifr.ifr_name[IFNAMSIZ-1] = '\0';
-	if(ioctl(s, SIOCGIFFLAGS, &ifr, &ifrlen) < 0)
-	{
+	if(ioctl(s, SIOCGIFFLAGS, &ifr, &ifrlen) < 0) {
+		if (errno == ENXIO || errno == ENODEV) {
+			/* Device not configured */
+			r = GETIFADDR_DEVICE_NOT_CONFIGURED;
+		} else {
+			r = GETIFADDR_IOCTL_ERROR;
+		}
 		syslog(LOG_DEBUG, "ioctl(s, SIOCGIFFLAGS, ...): %m");
 		close(s);
-		return -1;
+		return r;
 	}
-	if ((ifr.ifr_flags & IFF_UP) == 0)
-	{
+	if ((ifr.ifr_flags & IFF_UP) == 0) {
 		syslog(LOG_DEBUG, "network interface %s is down", ifname);
 		close(s);
-		return -1;
+		return GETIFADDR_IF_DOWN;
 	}
 	strncpy(ifr.ifr_name, ifname, IFNAMSIZ-1);
 	ifr.ifr_name[IFNAMSIZ-1] = '\0';
-	if(ioctl(s, SIOCGIFADDR, &ifr, &ifrlen) < 0)
-	{
+	if(ioctl(s, SIOCGIFADDR, &ifr, &ifrlen) < 0) {
+		if (errno == EADDRNOTAVAIL) {
+			/* Can't assign requested address */
+			r = GETIFADDR_NO_ADDRESS;
+		} else {
+			r = GETIFADDR_IOCTL_ERROR;
+		}
 		syslog(LOG_ERR, "ioctl(s, SIOCGIFADDR, ...): %m");
 		close(s);
-		return -1;
+		return r;
 	}
 	ifaddr = (struct sockaddr_in *)&ifr.ifr_addr;
 	if(addr) *addr = ifaddr->sin_addr;
-	if(buf)
-	{
-		if(!inet_ntop(AF_INET, &ifaddr->sin_addr, buf, len))
-		{
+	if(buf) {
+		if(!inet_ntop(AF_INET, &ifaddr->sin_addr, buf, len)) {
 			syslog(LOG_ERR, "inet_ntop(): %m");
 			close(s);
-			return -1;
+			return GETIFADDR_INET_NTOP_ERROR;
 		}
 	}
-	if(mask)
-	{
+	if(mask) {
 		strncpy(ifr.ifr_name, ifname, IFNAMSIZ-1);
 		ifr.ifr_name[IFNAMSIZ-1] = '\0';
-		if(ioctl(s, SIOCGIFNETMASK, &ifr, &ifrlen) < 0)
-		{
+		if(ioctl(s, SIOCGIFNETMASK, &ifr, &ifrlen) < 0) {
 			syslog(LOG_ERR, "ioctl(s, SIOCGIFNETMASK, ...): %m");
 			close(s);
-			return -1;
+			return GETIFADDR_IOCTL_ERROR;
 		}
 #ifdef ifr_netmask
 		*mask = ((struct sockaddr_in *)&ifr.ifr_netmask)->sin_addr;
@@ -104,14 +109,12 @@ getifaddr(const char * ifname, char * buf, int len,
 	struct ifaddrs * candidate = NULL;
 
 	if(!ifname || ifname[0]=='\0')
-		return -1;
-	if(getifaddrs(&ifap)<0)
-	{
+		return GETIFADDR_BAD_ARGS;
+	if(getifaddrs(&ifap) < 0) {
 		syslog(LOG_ERR, "getifaddrs: %m");
-		return -1;
+		return GETIFADDR_GETIFADDRS_ERROR;
 	}
-	for(ife = ifap; ife; ife = ife->ifa_next)
-	{
+	for(ife = ifap; ife; ife = ife->ifa_next) {
 		/* skip other interfaces if one was specified */
 		if(ifname && (0 != strcmp(ifname, ife->ifa_name)))
 			continue;
@@ -137,26 +140,22 @@ getifaddr(const char * ifname, char * buf, int len,
 */
 		}
 	}
-	if(candidate)
-	{
-		if(buf)
-		{
+	if(candidate) {
+		if(buf) {
 			inet_ntop(candidate->ifa_addr->sa_family,
 			          &((struct sockaddr_in *)candidate->ifa_addr)->sin_addr,
 			          buf, len);
 		}
 		if(addr) *addr = ((struct sockaddr_in *)candidate->ifa_addr)->sin_addr;
 		if(mask) *mask = ((struct sockaddr_in *)candidate->ifa_netmask)->sin_addr;
-	}
-	else
-	{
+	} else {
 		syslog(LOG_WARNING, "no AF_INET address found for %s", ifname);
 		freeifaddrs(ifap);
-		return -1;
+		return GETIFADDR_NO_ADDRESS;
 	}
 	freeifaddrs(ifap);
 #endif
-	return 0;
+	return GETIFADDR_OK;
 }
 
 #ifdef ENABLE_PCP

miniupnpd/getifaddr.h
@@ -1,4 +1,4 @@
-/* $Id: getifaddr.h,v 1.12 2025/04/03 21:11:35 nanard Exp $ */
+/* $Id: getifaddr.h,v 1.13 2025/04/08 21:28:42 nanard Exp $ */
 /* vim: tabstop=4 shiftwidth=4 noexpandtab
  * MiniUPnP project
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
@@ -13,6 +13,24 @@
  * \brief get IP address of network interface
  */
 
+#define GETIFADDR_OK 0
+/*! \brief bad arguments */
+#define GETIFADDR_BAD_ARGS -1
+/*! \brief socket() call error */
+#define GETIFADDR_SOCKET_ERROR -2
+/*! \brief device not configure / no such device */
+#define GETIFADDR_DEVICE_NOT_CONFIGURED -3
+/*! \brief other error from ioctl() */
+#define GETIFADDR_IOCTL_ERROR -4
+/*! \brief network interface is down */
+#define GETIFADDR_IF_DOWN -5
+/*! \brief no IPv4 address for the network interface */
+#define GETIFADDR_NO_ADDRESS -6
+/*! \brief inet_ntop() call error */
+#define GETIFADDR_INET_NTOP_ERROR -7
+/*! \brief getifaddrs() call error */
+#define GETIFADDR_GETIFADDRS_ERROR -8
+
 struct in_addr;
 struct in6_addr;
 
@@ -24,7 +42,7 @@ struct in6_addr;
  * \param[in] len buf length
  * \param[out] addr ip v4 address
  * \param[out] mask ip v4 mask
- * \return 0 on success, -1 on failure */
+ * \return 0 on success, a negative value on failure */
 int
 getifaddr(const char * ifname, char * buf, int len,
           struct in_addr * addr, struct in_addr * mask);

miniupnpd/miniupnpd.c
@@ -1,4 +1,4 @@
-/* $Id: miniupnpd.c,v 1.267 2025/04/06 22:30:22 nanard Exp $ */
+/* $Id: miniupnpd.c,v 1.268 2025/04/08 21:28:42 nanard Exp $ */
 /* vim: tabstop=4 shiftwidth=4 noexpandtab
  * MiniUPnP project
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
@@ -941,24 +941,39 @@ parselanaddr(struct lan_addr_s * lan_addr, const char * str, int debug_flag)
 	while(*p && *p != '/' && !isspace(*p))
 		p++;
 	n = p - str;
-	if(!isdigit(str[0]) && n < (int)sizeof(lan_addr->ifname))
-	{
+	if(!isdigit(str[0]) && n < (int)sizeof(lan_addr->ifname)) {
+		int r;
 		/* not starting with a digit : suppose it is an interface name */
 		memcpy(lan_addr->ifname, str, n);
 		lan_addr->ifname[n] = '\0';
-		if(getifaddr(lan_addr->ifname, lan_addr->str, sizeof(lan_addr->str),
-		             &lan_addr->addr, &lan_addr->mask) < 0) {
+		r = getifaddr(lan_addr->ifname, lan_addr->str, sizeof(lan_addr->str),
+		             &lan_addr->addr, &lan_addr->mask);
 #ifdef ENABLE_IPV6
+		if(r == GETIFADDR_NO_ADDRESS) {
 			fprintf(stderr, "interface \"%s\" has no IPv4 address\n", str);
 			syslog(LOG_NOTICE, "interface \"%s\" has no IPv4 address\n", str);
 			lan_addr->str[0] = '\0';
 			lan_addr->addr.s_addr = htonl(0x00000000u);
 			lan_addr->mask.s_addr = htonl(0xffffffffu);
-#else /* ENABLE_IPV6 */
+		} else if(r != GETIFADDR_OK) {
+			INIT_PRINT_ERR("error getting address for interface %s\n", str);
 			goto parselan_error;
-#endif /* ENABLE_IPV6 */
 		}
-		/*printf("%s => %s\n", lan_addr->ifname, lan_addr->str);*/
+#else /* ENABLE_IPV6 */
+		if(r == GETIFADDR_NO_ADDRESS) {
+			INIT_PRINT_ERR("interface \"%s\" has no address\n", str);
+			goto parselan_error;
+		} else if (r == GETIFADDR_DEVICE_NOT_CONFIGURED) {
+			INIT_PRINT_ERR("interface \"%s\" is not configured\n", str);
+			goto parselan_error;
+		} else if (r == GETIFADDR_IF_DOWN) {
+			INIT_PRINT_ERR("interface \"%s\" is down\n", str);
+			goto parselan_error;
+		} else if(r != GETIFADDR_OK) {
+			INIT_PRINT_ERR("error getting address for interface %s\n", str);
+			goto parselan_error;
+		}
+#endif /* ENABLE_IPV6 */
 	}
 	else
 	{
@@ -1136,8 +1151,52 @@ int update_ext_ip_addr_from_stun(int init)
 	return 0;
 }
 
+/*! \brief check external IP address and update disable_port_forwarding
+ */
+static void update_disable_port_forwarding(void)
+{
+	char if_addr[INET_ADDRSTRLEN];
+	struct in_addr addr;
+	int r = getifaddr(ext_if_name, if_addr, INET_ADDRSTRLEN, &addr, NULL);
+	if (r < 0) {
+		switch(r) {
+		case GETIFADDR_DEVICE_NOT_CONFIGURED:
+			syslog(LOG_WARNING, "ext interface %s is not configured / no such device", ext_if_name);
+			break;
+		case GETIFADDR_IF_DOWN:
+			syslog(LOG_WARNING, "ext interface %s is down", ext_if_name);
+			break;
+		case GETIFADDR_NO_ADDRESS:
+			syslog(LOG_WARNING, "ext interface %s has no IPv4 address. Network is down", ext_if_name);
+			break;
+		default:
+			syslog(LOG_ERR, "Error getting IPv4 address for ext interface %s. Network is down", ext_if_name);
+		}
+		disable_port_forwarding = 1;
+	} else {
+		int reserved = addr_is_reserved(&addr);
+		if (!disable_port_forwarding && reserved) {
+			if (GETFLAG(IGNOREPRIVATEIPMASK)) {
+				syslog(LOG_WARNING, "IGNORED : Reserved / private IP address %s on ext interface %s", if_addr, ext_if_name);
+			} else {
+				syslog(LOG_WARNING, "Reserved / private IP address %s on ext interface %s: Port forwarding is impossible", if_addr, ext_if_name);
+				syslog(LOG_INFO, "You are probably behind NAT, enable option ext_perform_stun=yes to detect public IP address");
+				syslog(LOG_INFO, "Or use ext_ip= / -o option to declare public IP address");
+				syslog(LOG_INFO, "In case that miniupnpd is thinking that it's behind symmetric NAT while it actually is full-cone");
+				syslog(LOG_INFO, "You can set option ignore_private_ip_check=yes to enable port forwarding");
+				syslog(LOG_INFO, "But you may still need to configure stun server or ext_ip to make it work correctly");
+				syslog(LOG_INFO, "Public IP address is required by UPnP/PCP/PMP protocols and clients do not work without it");
+				disable_port_forwarding = 1;
+			}
+		} else if (disable_port_forwarding && !reserved) {
+			syslog(LOG_INFO, "Public IP address %s on ext interface %s: Port forwarding is enabled", if_addr, ext_if_name);
+			disable_port_forwarding = 0;
+		}
+	}
+}
+
 /* fill uuidvalue_wan and uuidvalue_wcd based on uuidvalue_igd */
-void complete_uuidvalues(void)
+static void complete_uuidvalues(void)
 {
 	size_t len;
 	len = strlen(uuidvalue_igd);
@@ -2357,25 +2416,7 @@ main(int argc, char * * argv)
 	}
 	else if (!use_ext_ip_addr)
 	{
-		char if_addr[INET_ADDRSTRLEN];
-		struct in_addr addr;
-		if (getifaddr(ext_if_name, if_addr, INET_ADDRSTRLEN, &addr, NULL) < 0) {
-			syslog(LOG_WARNING, "Cannot get IP address for ext interface %s. Network is down", ext_if_name);
-			disable_port_forwarding = 1;
-		} else if (addr_is_reserved(&addr)) {
-			if (GETFLAG(IGNOREPRIVATEIPMASK)) {
-				syslog(LOG_WARNING, "IGNORED : Reserved / private IP address %s on ext interface %s", if_addr, ext_if_name);
-			} else {
-				syslog(LOG_WARNING, "Reserved / private IP address %s on ext interface %s: Port forwarding is impossible", if_addr, ext_if_name);
-				syslog(LOG_INFO, "You are probably behind NAT, enable option ext_perform_stun=yes to detect public IP address");
-				syslog(LOG_INFO, "Or use ext_ip= / -o option to declare public IP address");
-				syslog(LOG_INFO, "In case that miniupnpd is thinking that it's behind symmetric NAT while it actually is full-cone");
-				syslog(LOG_INFO, "You can set option ignore_private_ip_check=yes to enable port forwarding");
-				syslog(LOG_INFO, "But you may still need to configure stun server or ext_ip to make it work correctly");
-				syslog(LOG_INFO, "Public IP address is required by UPnP/PCP/PMP protocols and clients do not work without it");
-				disable_port_forwarding = 1;
-			}
-		}
+		update_disable_port_forwarding();
 	}
 
 #ifdef DYNAMIC_OS_VERSION
@@ -2685,31 +2726,7 @@ main(int argc, char * * argv)
 			}
 			else if (!use_ext_ip_addr)
 			{
-				char if_addr[INET_ADDRSTRLEN];
-				struct in_addr addr;
-				if (getifaddr(ext_if_name, if_addr, INET_ADDRSTRLEN, &addr, NULL) < 0) {
-					syslog(LOG_WARNING, "Cannot get IP address for ext interface %s. Network is down", ext_if_name);
-					disable_port_forwarding = 1;
-				} else {
-					int reserved = addr_is_reserved(&addr);
-					if (!disable_port_forwarding && reserved) {
-						if (GETFLAG(IGNOREPRIVATEIPMASK)) {
-							syslog(LOG_WARNING, "IGNORED : Reserved / private IP address %s on ext interface %s.", if_addr, ext_if_name);
-						} else {
-							syslog(LOG_WARNING, "Reserved / private IP address %s on ext interface %s: Port forwarding is impossible", if_addr, ext_if_name);
-							syslog(LOG_INFO, "You are probably behind NAT, enable option ext_perform_stun=yes to detect public IP address");
-							syslog(LOG_INFO, "Or use ext_ip= / -o option to declare public IP address");
-							syslog(LOG_INFO, "In case that miniupnpd is thinking that it's behind symmetric NAT while it actually is full-cone");
-							syslog(LOG_INFO, "You can set option ignore_private_ip_check=yes to enable port forwarding");
-							syslog(LOG_INFO, "But you may still need to configure stun server or ext_ip to make it work correctly");
-							syslog(LOG_INFO, "Public IP address is required by UPnP/PCP/PMP protocols and clients do not work without it");
-							disable_port_forwarding = 1;
-						}
-					} else if (disable_port_forwarding && !reserved) {
-						syslog(LOG_INFO, "Public IP address %s on ext interface %s: Port forwarding is enabled", if_addr, ext_if_name);
-						disable_port_forwarding = 0;
-					}
-				}
+				update_disable_port_forwarding();
 			}
 #ifdef ENABLE_NATPMP
 			if(GETFLAG(ENABLENATPMPMASK))

commit 220bbece152eeff0cc1a0f9684c952ea9b1ae1e2

fix: testgetifaddr.c when building without ipv6 support
broken by 7fc5b4d2a5e62eb9152441715922b9083c19ed12

miniupnpd/testgetifaddr.c
@@ -23,8 +23,8 @@ int main(int argc, char * * argv) {
 	char str_addr[64];
 	struct in_addr addr;
 	struct in_addr mask;
-#ifdef ENABLE_IPV6
 	int r;
+#ifdef ENABLE_IPV6
 	char str_addr6[64];
 #endif
 	if(argc < 2) {