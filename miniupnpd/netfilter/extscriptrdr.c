11cb0cdf8ba7bb04bfd8a964f29895babd602cd4
Lucas Ventura <lfventura@users.noreply.github.com>
2025-12-17 03:49:07 -0300

    Troquei fork()+execve() por posix_spawn().

--- miniupnpd/netfilter/extscriptrdr.c
+++ miniupnpd/netfilter/extscriptrdr.c
@@ -13,9 +13,12 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <time.h>
+#include <spawn.h>
 #include "../config.h"
 #include "../upnpglobalvars.h"
 #include "extscriptrdr.h"
+
+extern char **environ;
 
 /* proto_itoa()
  * convert IPPROTO_TCP or IPPROTO_UDP to string */
@@ -60,61 +63,35 @@ static int execute_external_script(const char *operation, const char **args, int
 	
 	syslog(LOG_ERR, "external script found and executable: %s, operation: %s", external_script_path, operation);
 	
-	pid = fork();
-	if (pid == -1) {
-		syslog(LOG_ERR, "fork() failed: %m");
+	/* Build argv array */
+	char **argv = malloc(sizeof(char*) * (arg_count + 4));
+	if (!argv) {
+		syslog(LOG_ERR, "malloc failed");
 		return -1;
 	}
-
-	if (pid == 0) {
-		/* Child process */
-		int test_errno;
-		
-		/* Test file access BEFORE anything else */
-		test_errno = access("/bin/bash", X_OK);
-		syslog(LOG_ERR, "CHILD: access(/bin/bash) = %d, errno=%d", test_errno, errno);
-		
-		test_errno = access(external_script_path, R_OK);
-		syslog(LOG_ERR, "CHILD: access(%s) = %d, errno=%d", external_script_path, test_errno, errno);
-		
-		/* Build full argv for bash: bash, script_path, operation, arg1, arg2, ..., NULL */
-		char **argv = malloc(sizeof(char*) * (arg_count + 4));
-		if (!argv) {
-			syslog(LOG_ERR, "CHILD: malloc failed");
-			exit(1);
-		}
-
-		argv[0] = "/bin/bash";
-		argv[1] = (char*)external_script_path;
-		argv[2] = (char*)operation;
-		for (int i = 0; i < arg_count; i++) {
-			argv[i + 3] = (char*)args[i];
-		}
-		argv[arg_count + 3] = NULL;
-
-		syslog(LOG_ERR, "CHILD: about to execve /bin/bash");
-		
-		/* Build minimal environment */
-		char *envp[] = {
-			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-			"SHELL=/bin/bash",
-			NULL
-		};
-		
-		/* Close syslog RIGHT before exec */
-		closelog();
-		
-		/* Execute bash with script and all arguments */
-		execve("/bin/bash", argv, envp);
-		
-		/* If we get here, exec failed */
-		openlog("miniupnpd", LOG_PID | LOG_CONS, LOG_DAEMON);
-		syslog(LOG_ERR, "CHILD: execve failed: errno=%d", errno);
-		exit(254);
+	
+	argv[0] = "/bin/bash";
+	argv[1] = (char*)external_script_path;
+	argv[2] = (char*)operation;
+	for (int i = 0; i < arg_count; i++) {
+		argv[i + 3] = (char*)args[i];
 	}
-
-	/* Parent process */
+	argv[arg_count + 3] = NULL;
+	
+	syslog(LOG_ERR, "attempting posix_spawn");
+	
+	/* Use posix_spawn instead of fork+exec */
+	int spawn_result = posix_spawn(&pid, "/bin/bash", NULL, NULL, argv, environ);
+	
+	free(argv);
+	
+	if (spawn_result != 0) {
+		syslog(LOG_ERR, "posix_spawn failed: %s", strerror(spawn_result));
+		return -1;
+	}
+	
 	syslog(LOG_ERR, "parent: waiting for child process %d", pid);
+	
 	if (waitpid(pid, &status, 0) == -1) {
 		syslog(LOG_ERR, "waitpid() failed: %m");
 		return -1;
@@ -137,9 +114,7 @@ static int execute_external_script(const char *operation, const char **args, int
 
 	syslog(LOG_ERR, "external script finished with unknown status %d", status);
 	return -1;
-}
-
-/* ext_add_redirect_rule2()
+}/* ext_add_redirect_rule2()
  * Add port forwarding rule using external script
  * Script is called as: script add_redirect ifname rhost eport iaddr iport proto desc timestamp */
 int
